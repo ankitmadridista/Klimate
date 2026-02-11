b0f71fd825aefa19a92bb820f0e9433fa3a5260b
ankitmadridista <ankit.suravkar4@gmail.com>
2026-02-11 20:55:07 +0530

    renamed file to fix import issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "./components/ui/sonner";
 import { WeatherDashboard } from "./pages/weather-dashboard";
-import { Layout } from "./components/Layout";
+import { Layout } from "./components/layout";
 import { ThemeProvider } from "./context/theme-provider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CityPage } from "./pages/city-page";
